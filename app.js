f9a8d69284ab67ac60c74ea6cdffb9c82b2ab1e0
Shubham Ranga <51476691+shubhamrangaa@users.noreply.github.com>
2021-04-28 01:29:58 +0530

    working ops

--- app.js
+++ app.js
@@ -14,25 +14,75 @@ class Calculator {
     if (number === "." && this.currentOperand.includes(".")) return;
     this.currentOperand += number;
   }
+
   chooseOperation(operation) {
     if (this.currentOperand === "") return;
     if (this.prevOperand !== "") {
-      compute();
+      this.compute();
     }
+
     this.operation = operation;
     this.prevOperand = this.currentOperand;
     this.currentOperand = "";
+    if (this.operation === "Sin" || this.operation === "Cos") this.compute();
   }
 
   compute() {
     let computation;
     const prev = parseFloat(this.prevOperand);
     const current = parseFloat(this.currentOperand);
+    if (this.operation === "Cos") {
+      computation = Math.cos(prev);
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.prevOperand = "";
+      return;
+    }
+    if (this.operation === "Sin") {
+      computation = Math.sin(prev);
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.prevOperand = "";
+      return;
+    }
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (this.operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "/":
+        computation = prev + current;
+        break;
+      case "Sin":
+        computation = Math.sin(current);
+        break;
+      case "Cos":
+        computation = Math.cos(current);
+        break;
+      default:
+        break;
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.prevOperand = "";
   }
 
   updateDisplay() {
-    this.display.innerText =
-      this.prevOperand.toString() + this.currentOperand.toString();
+    if (this.operation) {
+      this.display.innerText =
+        this.prevOperand.toString() +
+        this.operation +
+        this.currentOperand.toString();
+    } else {
+      this.display.innerText =
+        this.prevOperand.toString() + this.currentOperand.toString();
+    }
   }
 }
 
@@ -53,7 +103,7 @@ numberButton.forEach((button) => {
 
 opButton.forEach((op) => {
   op.addEventListener("click", () => {
-    calculator.appendNumber(op.innerText);
+    calculator.chooseOperation(op.innerText);
     calculator.updateDisplay();
   });
 });
